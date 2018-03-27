eed21be0e4563d51624a8f1ad680621a5abb4623
Jonathan Whitehead <jonathan.whitehead@linkgroup.co.uk>
2018-03-27 12:59:27 +0100

    Fully working posh script

--- bin/Release/oraenv.ps1
+++ bin/Release/oraenv.ps1
@@ -27,70 +27,112 @@ If no sid is passed then:
     Set the new ORACLE_SID's environment, display the details & exit.
 #>
 
-param
+Param
 (  
-    [String]$NewSID
+    [String] $NewSID
 ) 
 
 function quit ()
 {
-    $env:NEW_SID = ''
-    $env:NEW_HOME = ''
-    $env:NEW_PATH = ''
-    $env:OLD_ORACLE_HOME = ''
-    $env:OLD_ORACLE_PATH = ''
     exit
 }
 
 function display_details ()
 {
-    Write-Output "ORACLE_SID = $env:ORACLE_SID"
-    Write-Output "ORACLE_HOME = $env:ORACLE_HOME"
-    Write-Output "NLS_DATE_FORMAT = $env:NLS_DATE_FORMAT"
-    Write-Output "NLS_LANG = $env:NLS_LANG"
+    Write-Output "ORACLE_SID=$env:ORACLE_SID"
+    Write-Output "ORACLE_HOME=$env:ORACLE_HOME"
+    Write-Output "NLS_DATE_FORMAT=$env:NLS_DATE_FORMAT"
+    Write-Output "NLS_LANG=$env:NLS_LANG `n"
 }
 
-#Write-Host 'New SID:' $NewSID
+#Write-Output 'New SID:' $NewSID
 
-$env:NEW_SID = ''
-$env:NEW_HOME = ''
-$env:NEW_PATH = ''
-$env:OLD_ORACLE_HOME = ''
-$env:OLD_ORACLE_PATH = ''
+$oraenv = @{}
 
 # Keep the old stuff we might be changing these.
 # However, only if we have current settings, which we might not!
 
-if (-not [string]::IsNullOrEmpty($env:ORACLE_HOME))
+if (-not [string]::IsNullOrEmpty($env:ORACLE_SID))
 {
-    $env:OLD_ORACLE_HOME = $env:ORACLE_HOME
-    $env:OLD_ORACLE_PATH = $env:ORACLE_PATH
+    $oraenv.Add("OldOracleHome", $env:ORACLE_HOME)
+    $oraenv.Add("OldOracleSID", $env:ORACLE_SID)
 }
 else {
-    $env:OLD_ORACLE_HOME = 'NOT_SET'
-    $env:OLD_ORACLE_PATH = 'NOT_SET'
+    $oraenv.Add("OldOracleHome", 'NOT_SET')
+    $oraenv.Add("OldOracleSID", 'NOT_SET')
 }
-
-#Write-Output $env:OLD_ORACLE_HOME
-#Write-Output $env:OLD_ORACLE_PATH
-
-# No SID supplied = display current details and exit, UNLESS
-# ORAENV_ASK is set to YES.
 
 # We have a SID? Skip the next bit.
 if (-not [string]::IsNullOrEmpty($NewSID))
 {
-    $env:NEW_SID = $NewSID
+    $oraenv.NewOracleSID = $NewSID
 }    
 else {
+    # No SID supplied = display current details and exit, UNLESS ORAENV_ASK is set to YES.
     if ($env:ORAENV_ASK -ne 'YES')
     {
-        Write-Output 'Current Environment details are:'
+        Write-Output "`nCurrent Environment details are:"
         display_details
         quit
     }
     else {
-        #Ask for a new ORACLE_SID, defaulting to the current one.
-        if (($NewSID = read-host "Enter a new value for ORACLE_SID [$env:OLD_ORACLE_SID]") -eq ''){$env:OLD_ORACLE_SID}else{$NewSID}
+        while ([string]::IsNullOrEmpty($NewSID))
+        {
+            #Ask for a new ORACLE_SID, defaulting to the current one.
+            $NewSID = read-host "Enter a new value for ORACLE_SID [$($oraenv.OldOracleSID)]"
+            if (([string]::IsNullOrEmpty($NewSID) -and $oraenv.OldOracleSID -ne "NOT_SET") -Or (-not [string]::IsNullOrEmpty($NewSID)))
+            {
+                $oraenv.NewOracleSID = $NewSID
+            } 
+        }
     }
-}    
+}
+
+# Get Oracle Home for the new sid
+$oraenv.NewOracleHome = cmd.exe /c ".\DBHome" $($oraenv.NewOracleSID) | Out-String -stream
+$oraenv.NewOracleHome -replace '(?:\s|\r|\n)',''
+
+
+if ([string]::IsNullOrEmpty($oraenv.NewOracleHome))
+{
+    Write-Output "Invalid SID supplied: $($oraenv.NewOracleHome)"
+    quit
+}
+
+
+
+#Do we need to change PATH? We will always have to change it
+#if this is the first time that ORACLE_SID or ORACLE_HOME are being set.
+if ($oraenv.OldOracleSID -eq "NOT_SET")
+{
+    $env:ORACLE_SID = $oraenv.NewOracleSID
+    $env:ORACLE_HOME = $oraenv.NewOracleHome
+    $env:PATH="$($oraenv.NewOracleHome)\bin;$env:PATH"
+} elseif ($oraenv.OldOracleHome -eq $oraenv.NewOracleHome) {
+    # No change to HOME (or SID perhaps), so PATH is ok, just set SID.
+    $env:ORACLE_SID = $oraenv.NewOracleSID    
+} else {
+    # So, we are here, we have to remove the old home from the path and add in the new one.
+    $oraenv.OldOracleHome = cmd.exe /c ".\DBHome" $($oraenv.OldOracleSID) | Out-String -stream
+    $oraenv.OldOracleHome -replace '(?:\s|\r|\n)',''
+
+    # Now remove the old oracle home from the path - if it's valid.
+    if (-not [string]::IsNullOrEmpty($oraenv.OldOracleHome)) {
+      $newpath = $env:PATH
+      # Remove unwanted elements
+      $newpath = ($newpath.Split(';') | Where-Object { $_ -ne "$($oraenv.OldOracleHome)\bin" }) -join ';'
+    }
+
+    #And finally, add the new oracle home to the path.
+    $env:ORACLE_SID = $oraenv.NewOracleSID
+    $env:ORACLE_HOME = $oraenv.NewOracleHome
+    $env:OLD_PATH = $env:PATH
+    $env:PATH="$($oraenv.NewOracleHome)\bin;$newpath"
+}
+
+# We always set these regardless.
+$env:NLS_DATE_FORMAT='yyyy/mm/dd hh24:mi:ss'
+$env:NLS_LANG='AMERICAN_AMERICA.WE8ISO8859P1'
+
+Write-Output "`nEnvironment set as follows:"
+display_details
